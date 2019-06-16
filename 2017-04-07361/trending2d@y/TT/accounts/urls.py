0d47d7198f3677a2637607210e9cf357731ffc44
William Kiluma <williamkiluma@gmail.com>
2019-06-16 20:14:59 +0300

    added an about page

--- 2017-04-07361/trending2d@y/TT/accounts/urls.py
+++ 2017-04-07361/trending2d@y/TT/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from .views import AboutPageView
 
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
